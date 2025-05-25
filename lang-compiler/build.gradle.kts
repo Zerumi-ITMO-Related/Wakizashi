9b43751a9ba07441d16c4bb7f7cddf20ce6dc2c9
Zerumi <afanasyev09032005@gmail.com>
2025-05-25 18:38:11 +0300

    feat: add compiler artifact for unite front & back-end

---
+++ lang-compiler/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "io.github.zerumi"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.ajalt.clikt:clikt:5.0.1")
+    implementation("com.github.ajalt.clikt:clikt-markdown:5.0.1")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
